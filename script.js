48f9c376f66cf882da7ebd718f1098b0184d6a6a
manoj <seemanoj2k@gmail.com>
2022-07-27 10:57:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let h3=document.querySelector('.h3');
+let count=10;
+h3.innerText=count--;
+setTimeout(()=>{
+    h3.innerText=count--;
+    setTimeout(()=>{
+         h3.innerText=count--;
+         setTimeout(()=>{
+             h3.innerText=count--;
+             setTimeout(()=>{
+                h3.innerText=count--;
+                 setTimeout(()=>{
+                     h3.innerText=count--;
+                    setTimeout(()=>{
+                        h3.innerText=count--;
+                        setTimeout(()=>{
+                            h3.innerText=count--;
+                            setTimeout(()=>{
+                                h3.innerText=count--;
+                                setTimeout(()=>{
+                                    h3.innerText=count--;
+                                    setTimeout(()=>{
+                                        h3.innerText="HAPPY INDEPENDENCE DAY"
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+             },1000)
+         },1000)
+    },1000)
+},1000)
